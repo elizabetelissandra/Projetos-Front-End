b8f08f5241a511e202f3e7c26a5a5ab389971f12
elizabetelissandra <elizabetelissandrasilva@gmail.com>
2023-12-20 10:43:25 -0300

    update app.js, 'Site de agencia digital/index.html' and style.css

--- Site de agencia digital/app.js
+++ Site de agencia digital/app.js
@@ -1,10 +1,19 @@
+function vermais(){
+
 const botao = document.getElementById('botao');
 const texto = document.querySelector('.textoescondido');
 
 botao.addEventListener('click', function() {
   if (texto.classList.contains('texto-visivel')) {
     texto.classList.remove('texto-visivel');
+    setTimeout(function() {
+      texto.style.display = 'none'; // Oculta o elemento após a transição
+    }, 50); // Tempo correspondente à duração da transição (500 milissegundos)
   } else {
-    texto.classList.add('texto-visivel');
+    texto.style.display = 'block'; // Mostra o elemento antes de alterar a opacidade
+    setTimeout(function() {
+      texto.classList.toggle('texto-visivel');
+    },100); // Espera um curto período antes de adicionar a classe
   }
 });
+}
