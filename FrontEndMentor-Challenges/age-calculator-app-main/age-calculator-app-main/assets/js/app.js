32d51abd5d07e6191a5764e5899c02d2dc653557
elizabetelissandra <elizabetelissandrasilva@gmail.com>
2024-02-09 19:41:00 -0300

    update style.css, app.js and FrontEndMentor-Challenges/age-calculator-app-main/age-calculator-app-main/index.html

--- FrontEndMentor-Challenges/age-calculator-app-main/age-calculator-app-main/assets/js/app.js
+++ FrontEndMentor-Challenges/age-calculator-app-main/age-calculator-app-main/assets/js/app.js
@@ -1,18 +1,24 @@
-const formulario = document.querySelector("#formulario")
+const formulario = document.querySelector("#formulario");
 
-formulario.addEventListener("submit",  (e) => {
-    e.preventDefault()
-    // Pegar as informações do usuário
-    const inputDia = document.querySelector(`#dia`).value;
-    const inputMes = document.querySelector(`#mes`).value;
-    const inputAno = document.querySelector(`#ano`).value;
-    
-    //calcular idade pela data de nascimento
-    const resultadoAno = 2023-inputAno;
+const resultados = (inputDia, inputMes, inputAno) => {
+    const resultadoAno = 2024-inputAno;
     const resultadoMes = 12-inputMes;
     const resultadoDia = 30-inputDia;
 
-    const resultAno = document.querySelector('.resultAno').innerHTML= `${resultadoAno}`
-    const resultMes = document.querySelector('.resultMes').innerHTML= `${resultadoMes}`
-    const resultDia = document.querySelector('.resultDia').innerHTML= `${resultadoDia}`
-})
+    document.querySelector('.resultAno').innerHTML= `${resultadoAno}`
+    document.querySelector('.resultMes').innerHTML= `${resultadoMes}`
+    document.querySelector('.resultDia').innerHTML= `${resultadoDia}`
+}
+
+formulario.addEventListener("submit", e => {
+  e.preventDefault();
+  // Pegar as informações do formulario
+  const inputDia = document.querySelector(`#dia`).value;
+  const inputMes = document.querySelector(`#mes`).value;
+  const inputAno = document.querySelector(`#ano`).value;
+
+  
+    resultados(inputDia, inputMes, inputAno);
+});
+
+
